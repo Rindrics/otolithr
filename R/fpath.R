3d8c1f1fe3184adc2d908856a971a14cf67404fc
Akira Hayashi <akira.hayashi.1987@gmail.com>
2019-04-24 11:07:46 +0900

    Add exclude function

--- R/fpath.R
+++ R/fpath.R
@@ -17,8 +17,8 @@ fullpath2fname <- function(full.path) {
 }
 
 get_dir2load <- function(paths) {
-  regex         <- ".+\\/data\\/.+\\.hdr$"
-  (match_length <- purrr::map(regex, gregexpr, paths) %>% unlist())
+  regex         <- "^(?!.*exclude).+\\/data\\/.+\\.hdr$"
+  (match_length <- purrr::map(regex, gregexpr, paths, perl = TRUE) %>% unlist())
   match_pos     <- which(match_length > 0)
   dir2load      <- paths[match_pos]
   dir2load
