2249816c8f6b07d15fb763245d0c05b50e00690a
Akira Hayashi <smxshxishxad@me.com>
2020-03-12 12:25:15 +0900

    :bug: Remove method dispatch
    Fix #23

--- R/preview.R
+++ R/preview.R
@@ -5,7 +5,6 @@
 #'  made by \code{hdr2df} and \code{hdr2df_in_dir}
 #' @return Nothing. Side-effect: plots graphs
 preview <- function(df_otolith) {
-  NextMethod("plot")
   p <- df_otolith %>%
     dplyr::group_by(ID, Age, BL_mm) %>%
     dplyr::summarize(OR_microm = max(OR_microm)) %>%
